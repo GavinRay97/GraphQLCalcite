648e0ac4f6810a3c360d13a03e6597c33406de4b
Gavin Ray <ray.gavin97@gmail.com>
2022-02-12 13:03:15 -0500

    tweak formatting of ForeignKeyManager

--- src/main/kotlin/ForeignKeyManager.kt
+++ src/main/kotlin/ForeignKeyManager.kt
@@ -4,10 +4,18 @@ import com.google.common.graph.NetworkBuilder
 @Suppress("UnstableApiUsage")
 object ForeignKeyManager {
     val foreignKeyGraph: MutableNetwork<FullyQualifiedTableName, ForeignKey> =
-        NetworkBuilder.directed().allowsSelfLoops(false).allowsParallelEdges(false).build()
+        NetworkBuilder
+            .directed()
+            .allowsSelfLoops(false)
+            .allowsParallelEdges(false)
+            .build()
 
     fun addForeignKey(foreignKey: ForeignKey) {
-        foreignKeyGraph.addEdge(foreignKey.sourceTable, foreignKey.targetTable, foreignKey)
+        foreignKeyGraph.addEdge(
+            foreignKey.sourceTable,
+            foreignKey.targetTable,
+            foreignKey
+        )
     }
 
     fun getForeignKeysForTable(table: FullyQualifiedTableName): Set<ForeignKey> {
